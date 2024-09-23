9b6274588100b0e264e82e47ad51205bad583256
irizow <irisrossell@gmail.com>
2024-09-23 17:19:20 +0300

    change to dark theme by default

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [darkTheme, setDarkTheme ] = useState(false)
+  const [darkTheme, setDarkTheme ] = useState(true)
 
   return (
     <>
