23381151f3d0a7f682ffaf2c4e905f6b208405e6
irizow <irisrossell@gmail.com>
2024-09-24 01:19:31 +0300

    fix style bugs

--- src/components/CompScreen/CompScreen.jsx
+++ src/components/CompScreen/CompScreen.jsx
@@ -1,21 +1,11 @@
 import ScreenFooter from "./ScreenFooter";
 import CompWindow from "./CompWindow";
-import { useState } from "react";
 import styles from './compscreen.module.css'
 
 export default function CompScreen({darkTheme, setDarkTheme}) {
-    
-    const [isSmllWndw, setIsSmllWndw] = useState(false);
-    const [component, setComponent] = useState(null);
-
-    function handleClick(element) {
-        setComponent(element)
-        setIsSmllWndw(true)
-    }
-
     return (
         <>
-        <CompWindow darkTheme={darkTheme} isSmllWndw={isSmllWndw} handleClick={handleClick} setIsSmllWndw={setIsSmllWndw} component={component}/>
+        <CompWindow darkTheme={darkTheme}/>
         <ScreenFooter darkTheme={darkTheme} setDarkTheme={setDarkTheme} />
         </>
     )
