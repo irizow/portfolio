d2365e08d3ed4f6d9e2cedc5916e4c018cca155c
irizow <irisrossell@gmail.com>
2024-10-07 12:40:23 +0300

    add animations to gallery

--- src/components/Stack/Gallery.jsx
+++ src/components/Stack/Gallery.jsx
@@ -2,19 +2,38 @@ import styles from './gallery.module.css'
 import prevArrow from '../../assets/images/previous.png'
 import nextArrow from '../../assets/images/next.png'
 import { galleryImgs } from '../../assets/gallery/gallery'
-
+import { motion } from 'framer-motion'
 import { useEffect, useState } from 'react'
+import { useSwipeable } from 'react-swipeable';
 
 
 export default function Gallery() {
 
+    const handlers = useSwipeable({
+        onSwipedDown: ()=> handleArrows(-1),
+        onSwipedUp: ()=> handleArrows(1),
+    })
 
+    const preloadImages = (imageArray) => {
+        return Promise.all(
+          imageArray.map((image) => {
+            return new Promise((resolve) => {
+              const img = new Image();
+              img.src = image.src;
+              img.onload = resolve;
+            });
+          })
+        );
+      };
+
+    const [imagesLoaded, setImagesLoaded] = useState(false);
     const [index, setIndex] = useState(0);
     const [images, setImages] = useState({
         prev: galleryImgs[galleryImgs.length - 1],
         curr: galleryImgs[0],
         next: galleryImgs[1]
     })
+    const [isAnimating, setIsAnimating] = useState(false);
 
     const updateImages = (newIndex) => {
         const prevIndex = (newIndex - 1 + galleryImgs.length) % galleryImgs.length;
@@ -25,27 +44,42 @@ export default function Gallery() {
             next: galleryImgs[nextIndex]
         });
     };
-
     const handleArrows = (n) => {
+        if (isAnimating || !imagesLoaded) return;
+    
+        setIsAnimating(true);
         const newIndex = (index + n + galleryImgs.length) % galleryImgs.length;
-        setIndex(newIndex);
-        updateImages(newIndex);
-    }
+    
+        const timeoutId = setTimeout(() => {
+          setIndex(newIndex);
+          updateImages(newIndex);
+          setIsAnimating(false);
+        }, 300);
+    
+      
+        return () => clearTimeout(timeoutId);
+      };
 
+      useEffect(() => {
+        preloadImages(galleryImgs).then(() => {
+          setImagesLoaded(true);
+          console.log('All images preloaded');
+        });
+      }, []);
 
 
     return (
         <div className={styles.container}>
             <div className={styles.header}>Photo Gallery</div>
-            <div className={styles.carrousel}>
-                <div className={`${styles.boxes} ${styles.smallbox}`}>
-                <img loading='lazy' src={images.prev.src} alt={images.prev.alt}></img>
+            <div className={styles.carrousel} {...handlers}>
+                <div className={`${styles.boxes} ${styles.smallbox} `}>
+                    <img className={`${isAnimating ? styles.animate : ''}`} src={images.prev.src} alt={images.prev.alt}></img>
                 </div>
                 <div className={`${styles.boxes} ${styles.bigbox}`}>
-                    <img loading='lazy' src={images.curr.src} alt={images.curr.alt}></img>
+                    <img className={`${isAnimating ? styles.animate : ''}`} src={images.curr.src} alt={images.curr.alt}></img>
                 </div>
                 <div className={`${styles.boxes} ${styles.smallbox}`}>
-                    <img loading='lazy' src={images.next.src} alt={images.next.alt}></img>
+                    <img className={`${isAnimating ? styles.animate : ''}`} src={images.next.src} alt={images.next.alt}></img>
                 </div>
                 
             </div>
