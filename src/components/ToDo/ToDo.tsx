e6a9bbdc8d2e1849641250c2d7e4a6e5fb3fe989
irizow <irisrossell@gmail.com>
2026-02-24 18:36:23 +0100

    add progress bar and style

--- src/components/ToDo/ToDo.tsx
+++ src/components/ToDo/ToDo.tsx
@@ -1,6 +1,7 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "./todo.module.css";
 import { v1 as uuidv1 } from "uuid";
+import trashIcon from "../../assets/icons/trash.png";
 
 const PRIORITIES = ["low", "medium", "high"] as const;
 
@@ -15,12 +16,23 @@ type Todo = {
 
 export const ToDo = () => {
   const [todos, setTodos] = useState<Todo[]>([]);
+  const [error, setError] = useState<string>("");
+
+  const percentageDone =
+    todos.length === 0
+      ? 0
+      : (todos.filter((todo) => todo.status === "done").length * 100) /
+        todos.length;
+
   const [currTodo, setCurrTodo] = useState<{
     todo: string;
     priority: Priority;
   }>({ todo: "", priority: "low" });
 
   const handleAdd = () => {
+    setError("");
+    if (!currTodo.todo)
+      return setError("Ooops, looks like you tried to add an empty task");
     const id = uuidv1();
     console.log("about to add a todo with theid", id);
     setTodos((prev) => [
@@ -35,11 +47,13 @@ export const ToDo = () => {
   };
 
   const handleDelete = (id: string) => {
+    setError("");
     const newTodos = todos.filter((todo) => todo.id !== id);
     setTodos(newTodos);
   };
 
   const toggleStatus = (id: string) => {
+    setError("");
     setTodos((prevTodos) => {
       return prevTodos.map((prevTodo) =>
         prevTodo.id === id
@@ -53,20 +67,7 @@ export const ToDo = () => {
   };
   return (
     <section className={styles.tasks}>
-      <div className={styles.todolist}>
-        {todos.map((todo) => (
-          <div
-            key={todo.id}
-            className={`${todo.status === "done" ? styles.done : ""} ${styles.todorow} ${styles[todo.priority]}`}
-          >
-            <button onClick={() => toggleStatus(todo.id)}>
-              {todo.status === "done" && "x"}
-            </button>{" "}
-            <p>{todo.todo}</p>
-            <button onClick={() => handleDelete(todo.id)}>delete</button>
-          </div>
-        ))}
-      </div>
+      <h3>My ToDo's</h3>
       <div className={styles.todoinput}>
         <label>
           <input
@@ -90,8 +91,96 @@ export const ToDo = () => {
             ))}
           </select>
         </label>
+        {error && <span className={styles.error}>{error}</span>}
         <button onClick={handleAdd}>Add Todo</button>
       </div>
+      <div className={styles.todolist}>
+        {todos.map((todo) => (
+          <TodoRow
+            todo={todo}
+            toggleStatus={toggleStatus}
+            handleDelete={handleDelete}
+          />
+        ))}
+      </div>
+      <ProgressBar percentage={percentageDone} />
     </section>
+  );
+};
+
+const TodoRow = ({
+  todo,
+  toggleStatus,
+  handleDelete,
+}: {
+  todo: Todo;
+  toggleStatus: (id: string) => void;
+  handleDelete: (id: string) => void;
+}) => {
+  return (
+    <div
+      key={todo.id}
+      className={`${todo.status === "done" ? styles.done : ""} ${styles.todorow} ${styles[todo.priority]}`}
+    >
+      <button
+        className={styles.status_button}
+        onClick={() => toggleStatus(todo.id)}
+      >
+        {todo.status === "done" && "x"}
+      </button>{" "}
+      <p>{todo.todo}</p>
+      <button
+        className={styles.delete_button}
+        onClick={() => handleDelete(todo.id)}
+      >
+        <img alt="trash icon" src={trashIcon}></img>
+      </button>
+    </div>
+  );
+};
+
+const ProgressBar = ({ percentage }: { percentage: number }) => {
+  const color =
+    percentage <= 33 ? "red" : percentage < +66 ? "yellow" : "green";
+  const radius = 30;
+  const stroke = 8;
+  const normalizedRadius = radius - stroke / 2;
+  const circumference = 2 * Math.PI * normalizedRadius;
+
+  const strokeDashoffset = circumference - (percentage / 100) * circumference;
+
+  return (
+    <svg className={styles.progress_bar} height={radius * 2} width={radius * 2}>
+      <circle
+        stroke="#eee"
+        fill="transparent"
+        strokeWidth={stroke}
+        r={normalizedRadius}
+        cx={radius}
+        cy={radius}
+      />
+      <text
+        x="50%"
+        y="50%"
+        textAnchor="middle"
+        dominantBaseline="middle"
+        fontSize="20"
+        fontWeight="bold"
+      >
+        {percentage}%
+      </text>
+      <circle
+        stroke={color}
+        fill="transparent"
+        strokeWidth={stroke}
+        strokeDasharray={circumference}
+        strokeDashoffset={strokeDashoffset}
+        strokeLinecap="round"
+        r={normalizedRadius}
+        cx={radius}
+        cy={radius}
+        style={{ transition: "stroke-dashoffset 0.35s" }}
+      />
+    </svg>
   );
 };
